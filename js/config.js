9123491014d23b1be1c3ada7eb635e737c2f0fab
GDG Kansas City <gdgkansascity@gmail.com>
2014-03-19 01:50:11 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config', function () {
+    return {
+        //modify these
+        'name'          : 'GDG Kansas City',
+        'id'            : '116015988631052616691',
+        'google_api'    : 'AIzaSyDI0kLAo-_krd_XFsT3snJUWnAftTApVik',
+        'pwa_id'        : '', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'Dart Flight School',
+            subtitle : 'Dart hit 1.0 and is now Flight Ready! Developer communities around the world are hosting Dart Flight School events during February-March 2014.',
+            button : {
+                text : 'Find Local Event',
+                url : 'https://www.dartlang.org/events/2014/flight-school/'
+            }
+        }
+    };
+});
