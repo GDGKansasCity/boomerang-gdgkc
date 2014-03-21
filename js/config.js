2c31a4a64852cebf65d9f0ffa504308793fe35e6
Kyle Paul <gdgkansascity@gmail.com>
2014-03-21 22:00:40 +0000

    move lib folder into js directory and clean up yaml

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ boomerang.factory('Config', function () {
         'name'          : 'GDG Kansas City',
         'id'            : '116015988631052616691',
         'google_api'    : 'AIzaSyDI0kLAo-_krd_XFsT3snJUWnAftTApVik',
-        'pwa_id'        : '', //picasa web album id, must belong to google+ id above
+        'pwa_id'        : '109538469900571472833', //picasa web album id, must belong to google+ id above
         'cover' : {
             title : 'Dart Flight School',
             subtitle : 'Dart hit 1.0 and is now Flight Ready! Developer communities around the world are hosting Dart Flight School events during February-March 2014.',
